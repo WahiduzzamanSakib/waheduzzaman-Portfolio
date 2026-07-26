b8e3097a983aff8ad37d1c451f9f21b207245d16
WahiduzzamanSakib <wahidzamanpg@gmail.com>
2026-07-26 19:53:58 +0600

    Features Add

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
     <div>
        <Hero /> 
       <About />
-      {/* <Features /> */}
+      <Features />
 
       {/* <TechStack /> */}
       {/* <Skills /> */}
